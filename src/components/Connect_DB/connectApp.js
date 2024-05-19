bb1997cd9f4845a2dc3c87c67c1dc9daf284c3c5
Spirit <vkozyaychev@mail.ru>
2024-05-19 17:02:32 +0300

    add Sign_up and need fix photo

--- src/components/Connect_DB/connectApp.js
+++ src/components/Connect_DB/connectApp.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql')
+const bodyParser = require('body-parser')
 
 //введены изменения{
 const express = require('express');
@@ -7,6 +8,7 @@ const cors = require('cors')
 
 // app.use(express.json());
 app.use(cors())
+app.use(bodyParser())
 //введены изменения}
 
 const conn = mysql.createConnection({
@@ -16,24 +18,38 @@ const conn = mysql.createConnection({
     database: "WEB_service"
 })
 
-  //введены изменения{
-    app.get('/data', (req, res) => {
-        const query = 'SELECT * FROM Masters';
-        conn.query(query, (error, results) => {
-          if (error) {
-            console.error('Ошибка выполнения запроса:', error);
-            res.status(500).send('Ошибка сервера');
-            return;
-          }
-          console.log("тест ",results)
-          res.json(results);
-        });
+//введены изменения{
+app.get('/data', (req, res) => {
+    const query = 'SELECT * FROM Masters';
+    conn.query(query, (error, results) => {
+      if (error) {
+        console.error('Ошибка выполнения запроса:', error);
+        res.status(500).send('Ошибка сервера');
+        return;
+      }
+      console.log("тест ",results)
+      res.json(results);
+    });
+});
 
-      });
+app.post('/data', (req, res) => {
+  let data = [req.body.id, req.body.secondName, req.body.firstName, req.body.patronymic, req.body.phoneNumber, req.body.description, req.body.specializations, req.body.photo.file, req.body.quality, req.body.linkVK]
+  const query = 'INSERT INTO `Masters`(`ID`, `SecondName`, `FirstName`, `Patronymic`, `PhoneNumber`, `Description`, `Specialization`, `Images`, `Quality`, `LinkVK`) VALUES (?,?,?,?,?,?,?,?,?,?)';
+  conn.query(query, data, (error, results) => {
+    if (error) {
+      console.error('Ошибка выполнения запроса:', error);
+      res.status(500).send('Ошибка сервера');
+      return;
+    }
+    console.log("тест ",results)
+    console.log(req.body.photo.file)
+    res.json(results);
+  });
+});   
       
-      const port = process.env.PORT || 3001;
-      app.listen(port, () => {
-        console.log(`Сервер запущен на порту ${port}`);
-      });
-  //введены изменения}
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Сервер запущен на порту ${port}`);
+});
+//введены изменения}
 
--- src/components/Pages/Login/Sign_up.js
+++ src/components/Pages/Login/Sign_up.js
@@ -1,39 +1,34 @@
 import React, { useState } from "react";
+import axios from 'axios';
 import "./Sign_up.css";
 
 const Sign_up = () => {
-
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
-    }
-
     var user = new Object();
-    user.id = getRandomInt(99);
+    user.id = null;
     user.login = "";
     user.password = "";
     user.isWorker=false;
-    user.idClient =getRandomInt(99)
+    user.idClient = null;
 
-    var client = new Object();
-    client.id = user.idClient;
-    client.firstName ="";
-    client.secondName="";
-    client.patronymic="";
-    client.phoneNumber="";
-    client.linkVK=""
-    client.isWorker=false;
-    client.description="";
-    client.specializations="";
-    client.photo = 0;
+    const Sign = async () =>{
 
-    client.setPhoto = function(photo) {
-        this.photo = photo;
-    };
-    client.setSpecializations = function(specs) {
-        specs.map(spec =>{
-            this.specializations = this.specializations + "; " + spec;
-        })
-    };
+        //проблема с фотографиями надо пофиксить! !!!
+        const response = await axios.post('http://localhost:3001/data',{
+            id: null,
+            firstName: firstName,
+            secondName: secondName,
+            patronymic: patronymic,
+            phoneNumber: number,
+            linkVK: linkVK,
+            description: description,
+            specializations: textSpecializations,
+            photo: selectedFile,
+            quality: "5"
+        });
+        console.log(textSpecializations)
+        console.log(photo)
+        console.log(selectedFile)
+    }
 
     const [firstName, setFirstName] = useState("")
     const hChangeFirst = (e) =>{
@@ -48,7 +43,6 @@ const Sign_up = () => {
         setPatronymic(e.target.value);
     }
 
-    //надо пофиксить чекбокс... галочки не работают
     const [specializations, setSpecializations] = useState([])
     const checkMaster = (e) =>{
         let toggle = true
@@ -64,8 +58,15 @@ const Sign_up = () => {
             specializations.splice(index, 1)
             setSpecializations([...[],...specializations]);
         }
-        
+        textSpec();
     }
+    const [textSpecializations, setTextSpecializations] = useState("")
+    const textSpec = () =>{
+        if(!!specializations)specializations.map(
+            specialization => setTextSpecializations(textSpecializations+specialization+"; ")
+        )
+    }
+
     const [number, setNumber] = useState("");
     const hChangePhoneNumber = (e) =>{
         setNumber(e.target.value);
@@ -80,7 +81,8 @@ const Sign_up = () => {
     }
 
     const [description, setDescription] = useState("");
-    const handleChange = (e) => {
+
+    const handleDescription = (e) => {
         setDescription(e.target.value);
     }
 
@@ -99,23 +101,23 @@ const Sign_up = () => {
         }
     };
 
-    const fileUploadHandler = () => {
+    const [photo, setPhoto] = useState();
+
+    const fileUploadHandler = async () => {
         if (!selectedFile) {
             console.log("No file selected!");
             return;
         }
 
         const formData = new FormData();
-        formData.append('image', selectedFile);
+        formData.append('file', selectedFile);
+        //закидываем formdata в photo для передачи в объект передаваемый на api сервер
+        setPhoto(formData);
 
-        // Здесь отправьте formData на сервер, который ожидает longblob
 
+        console.log(formData);
         console.log("File ready for upload:", selectedFile);
     };
-
-    const Sign = () =>{
-
-    }
 
     const [togglerMaster, setTogglerMaster] = useState(false)
 
@@ -124,7 +126,7 @@ const Sign_up = () => {
         return(
             <div>
                 <h3>Описание: </h3>
-                <textarea onChange={handleChange}></textarea>
+                <textarea onChange={handleDescription}></textarea>
                     <h3>Специализации:</h3>
                     <div>
                         <input type="checkbox" id="painter" name="painter" onClick={checkMaster} />
@@ -183,7 +185,50 @@ const Sign_up = () => {
                     <label for="master">Вы мастер по ремеонту?</label>
                 </div>
 
-                {togglerMaster &&<Sign_Master/>}
+                {/* {togglerMaster && <Sign_Master/>} */}
+                <div>
+                <h3>Описание: </h3>
+                <textarea onChange={handleDescription}></textarea>
+                    <h3>Специализации:</h3>
+                    <div>
+                        <input type="checkbox" id="painter" name="painter" onClick={checkMaster} />
+                        <label for="painter">Маляр</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="electrician" name="electrician" onClick={checkMaster} />
+                        <label for="electrician">Электрик</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="plumber" name="plumber" onClick={checkMaster} />
+                        <label for="plumber">Сантехник</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="installer" name="installer" onClick={checkMaster} />
+                        <label for="installer">Мотажник</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="tiler" name="tiler" onClick={checkMaster} />
+                        <label for="tiler">Плиточник</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="ceilings" name="ceilings" onClick={checkMaster} />
+                        <label for="ceilings">Натежные потолки</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="doors" name="doors" onClick={checkMaster} />
+                        <label for="doors">Двери</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="decoration" name="decoration" onClick={checkMaster} />
+                        <label for="decoration">Отделка стен</label>
+                    </div>
+                    <div>
+                        <input type="file" onChange={fileSelectedHandler} accept="image/*" />
+                        {previewUrl && <img src={previewUrl} alt="Preview" style={{ maxWidth: '100%', maxHeight: '200px' }} />}
+                        {selectedFile && <button onClick={fileUploadHandler}>Upload</button>}
+                    </div>
+                </div>
+                {/* end */}
 
                 <button className="CustomButton" onClick={Sign}>Зарегистрировать</button>
 
